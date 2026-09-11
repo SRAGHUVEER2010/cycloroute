029a129e85572c88f163031d216a1451bd3017f2
SRAGHUVEER2010 <sraghuveer2010@zohomail.in>
2026-09-11 17:40:07 +0530

    Testing aerodynamic drag

--- cycloroute/physics.py
+++ cycloroute/physics.py
@@ -1,4 +1,5 @@
 import math
+from cycloroute.wind import relative_wind
 #predefining acceleration due to gravity
 g = 9.81
 #converting road gradient to radian angle measure
@@ -16,3 +17,8 @@ def rolling_resistance(angle_radians , mass , rolling_resistance_coefficient):
 
     rolling_force = mass * g * cos_angle * rolling_resistance_coefficient
     return rolling_force
+#Calculating Aerodynamic drag
+v_relative = relative_wind(wind_speed, wind_direction, cyclist_speed, cyclist_direction)
+def aero_drag(air_density,coefficient_of_drag,frontal_area,v_relative):
+    aerodynamic_drag  = 0.5 * air_density * coefficient_of_drag * frontal_area * v_relative ** 2
+    return aerodynamic_drag
